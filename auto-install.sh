68aee8c359f203c1750f84cfaef43f8525c428fb
jsah-mc <sah.joseph@outlook.com>
2026-08-10 21:07:28 -0400

    cds into current dir dir

--- auto-install.sh
+++ auto-install.sh
@@ -1,6 +1,8 @@
+CURRENT_DIR="$(pwd)"
 # This is to Install dotfiles without precloning
 sudo pacman -S git
 
 git clone https://github.com/jsah-mc/seam.git ~/.cache/seamdots
 cd ~/.cache/seamdots/
 ./install.sh
+cd $CURRENT_DIR
